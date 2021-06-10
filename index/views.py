2ed969bb237a750ef48419668045ecdd78a9e0de
Yunju <ooonjuuu@naver.com>
2021-06-11 01:04:17 +0900

    Create navbar using template inheritance

---
+++ index/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+
+def home(request):
+    welcome = "Welcome to HUFS Hotel"
+    return render(request, 'index/home.html', {'welcome' : welcome})
+
+def hotel(request):
+    hotel = 'HUFS'
+    return render(request, 'index/hotel.html', {'hotel' : hotel})
